56c03363c6779cf0171a3eb7c46c8d46cb5b6639
Gabriel Gotlib <gagotlib@gmail.com>
2023-09-08 11:41:18 +0200

    ejercicio 2 pass

--- 02.js
+++ 02.js
@@ -12,6 +12,13 @@ function stringMasLarga(strings) {
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
   // Tu código aca
+  var stringlargo = "a"
+  for ( let i = 0; i < strings.length; i++) {
+    if (strings[i].length > stringlargo.length) {
+      stringlargo = strings[i]
+    }
+  }
+  return stringlargo
 }
 
 // No modifiques nada debajo de esta linea //
